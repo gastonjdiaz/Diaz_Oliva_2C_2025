29e0cc15c761a34096ea6ecdc9b8724fa914a7f2
Gastón Díaz <diazgastonj@gmail.com>
2025-11-20 17:40:17 -0300

    Subo documentación

--- firmware/examples/proyecto_integrador/main/proyecto_integrador.c
+++ firmware/examples/proyecto_integrador/main/proyecto_integrador.c
@@ -23,9 +23,14 @@
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
  * | 22/10/2025 | Document creation		                         |
+ * | 19/11/2025 | Presentation			                         |
+ * | 20/11/2025 | Documentation			                         |
  *
  * @author Gastón Jair Díaz (diazgastonj@gmail.com)
  * @author Juan Ignacio Oliva (juaaaaanioliiiva@gmail.com)
+ * 
+ * @note Led y motor conectado a PWM con salidas de 5v de la placa
+ * @note PWM de led puesto a 100Hz, con 50 se veía el titileo
  */
 
 /*==================[inclusions]=============================================*/
@@ -40,22 +45,29 @@
 #include "hc_sr04.h"
 #include "servo_sg90.h"
 /*==================[macros and definitions]=================================*/
-#define DISTANCE_CLOSE  8	   // distancia en cm para modo de trabajo intenso
-#define DISTANCE_MEDIUM  16   // distancia en cm para modo de trabajo moderado
-#define DISTANCE_FAR  24	   // distancia en cm para modo de reposo
-#define DISTANCE_OFF  30	   // distancia en cm para modo apagado
-#define DUTY_CYCLE_MAX  100   // ciclo de trabajo maximo
-#define DUTY_CYCLE_MEDIUM  60 // ciclo de trabajo medio medio
-#define DUTY_CYCLE_LOW  30	   // ciclo de trabajo bajo
-#define DUTY_CYCLE_OFF  0	   // ciclo de trabajo apagado
-#define APERTURE_WIDE_OPEN -90   // apertura de iris maxima
-#define APERTURE_MEDIUM_OPEN 0 // apertura de iris media
-#define APERTURE_NARROW_OPEN 90  // apertura de iris minima
+/**
+ * @brief Lista constantes de configuración del proyecto
+ */
+#define DISTANCE_CLOSE  8	   ///<  distancia en cm para modo de trabajo intenso 
+#define DISTANCE_MEDIUM  16   ///<  distancia en cm para modo de trabajo moderado 
+#define DISTANCE_FAR  24	   ///<  distancia en cm para modo de reposo 
+#define DISTANCE_OFF  30	   ///<  distancia en cm para modo apagado 
+#define DUTY_CYCLE_MAX  100   ///<  ciclo de trabajo maximo  
+#define DUTY_CYCLE_MEDIUM  60 ///<  ciclo de trabajo medio medio 
+#define DUTY_CYCLE_LOW  30	   ///<  ciclo de trabajo bajo 
+#define DUTY_CYCLE_OFF  0	   ///<  ciclo de trabajo apagado 
+#define APERTURE_WIDE_OPEN -90   ///<  apertura de iris maxima 
+#define APERTURE_MEDIUM_OPEN 0 ///<  apertura de iris media 
+#define APERTURE_NARROW_OPEN 90  ///<  apertura de iris minima 
 
-#define CONFIG_OPERATION_CYCLE_MS 500
-#define CONFIG_MEASUREMENT_CYCLE_MS 200
+#define CONFIG_OPERATION_CYCLE_MS 500 ///<  Tiempo de período de operación en milisegundos 
+#define CONFIG_MEASUREMENT_CYCLE_MS 200 ///<  Tiempo de perído de medición en milisegundos 
 /*==================[internal data definition]===============================*/
 uint16_t _distancia;
+/**
+ * \def _medicionActivada
+ * \todo Variable para activar o desactivar modo manual de medición
+ */
 bool _medicionActivada = true;
 
 TaskHandle_t medir_task_handle = NULL;
@@ -64,7 +76,9 @@ TaskHandle_t AperturaHaz_task_handle = NULL;
 TaskHandle_t Uart_task_handle = NULL;
 
 
-
+/**
+ * @brief Mide distancia con HC-SR04 y actualiza variable global 
+ */
 static void Medir(void *pvParameter)
 {
 	while (true)
@@ -76,7 +90,9 @@ static void Medir(void *pvParameter)
 		vTaskDelay(pdMS_TO_TICKS(CONFIG_MEASUREMENT_CYCLE_MS));
 	}
 }
-
+/**
+ * @brief Envía datos de potencia y distancia por UART
+ */
 void EnviarDatosUART()
 {
 	while (true)
@@ -99,6 +115,9 @@ void EnviarDatosUART()
 	}
 }
 
+/**
+ * @brief Regula la intensidad de la luz con PWM dependiendo de la distancia medida
+ */
 void Regular_intensidad_luz()
 {
  while (true)
@@ -116,9 +135,10 @@ void Regular_intensidad_luz()
 	
 		vTaskDelay(pdMS_TO_TICKS(CONFIG_OPERATION_CYCLE_MS));
  }
- 
-
 }
+/**
+ * @brief Regula apartura del iris dependiendo de la distancia medida
+ */
 void Regular_apertura_haz()
 {
 	while (true)
@@ -137,6 +157,11 @@ void Regular_apertura_haz()
 	
 
 }
+/**
+ * @brief Función que acumula las otras pero sacada de uso
+ * 
+ * @deprecated Deprecado por no funionamiento (creemos que por los while sin el vTaskDelay)
+ */
 static void AjustarEInformar(void *pvParameter)
 {
 	while (true)
@@ -157,33 +182,13 @@ void app_main(void)
 	UART_USB.port = UART_PC;
 	UartInit(&UART_USB);
 	ServoInit(SERVO_1, GPIO_19); // Inicializa servo en GPIO19
-	PWMInit(PWM_2, GPIO_1, 100); // Inicializa PWM en GPIO5 a 50Hz -> revisar frecuencia adecuada para la luz.
+	PWMInit(PWM_2, GPIO_1, 100); // Inicializa PWM en GPIO5 a 50Hz -> revisar frecuencia adecuada para la luz. Eran 100 al final
 	HcSr04Init(GPIO_3, GPIO_2);
-
-	
-
-	// while (true)
-	// {
-	// 	int medicion = HcSr04ReadDistanceInCentimeters();
-	// 	UartSendString(UART_PC, (char*)UartItoa(medicion, 10));
-
-	// 	vTaskDelay(pdMS_TO_TICKS(1000));
-
-
-	// }
 	
 	xTaskCreate(Medir, "Medir", 2048, NULL, 5, &medir_task_handle);
 	xTaskCreate(Regular_apertura_haz, "AperturaHaz", 4096, NULL, 5, &AperturaHaz_task_handle);
 	xTaskCreate(Regular_intensidad_luz, "RegularLuz", 4096, NULL, 5, &PotenciaLuz_task_handle);
-	xTaskCreate(EnviarDatosUART, "EnviarDatos", 4096, NULL, 5, &Uart_task_handle);
-
-
-	// ServoMove(SERVO_1, APERTURE_WIDE_OPEN);    // Coloca servo en posicion inicial (0 grados)
-	// vTaskDelay(pdMS_TO_TICKS(1000));
-	// ServoMove(SERVO_1, APERTURE_MEDIUM_OPEN); 
-	// vTaskDelay(pdMS_TO_TICKS(1000));
-	// ServoMove(SERVO_1, APERTURE_NARROW_OPEN); 
-	
+	xTaskCreate(EnviarDatosUART, "EnviarDatos", 4096, NULL, 5, &Uart_task_handle);	
 
 }
 /*==================[end of file]============================================*/
